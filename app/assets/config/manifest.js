042ce2233eee44865319f97bcd2fa1fd266d5718
danfkelly <danielkellydev@gmail.com>
2025-01-10 09:50:27 +1100

    updated manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,2 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link application.js
-//= link tailwind.css
