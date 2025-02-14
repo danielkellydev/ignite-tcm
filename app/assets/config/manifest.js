29b6af3338348936b81273a1c6d06e305ac71cc9
danfkelly <danielkellydev@gmail.com>
2025-02-15 10:57:43 +1100

    updated manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link tailwind.css
+//= link_tree ../../javascript/controllers .js
 //= link application.js
-//= link controllers/application.js
-//= link controllers/index.js
-//= link controllers/mobile_menu_controller.js
+//= link tailwind.css
